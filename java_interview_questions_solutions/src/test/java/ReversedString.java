6cae724631a28d2f3299b20b4486a11864f4f3f2
Yusuf Bayram <yusufbayram19891989@gmail.com>
2022-12-23 18:47:49 +0000

    Add files via upload

---
+++ java_interview_questions_solutions/src/test/java/ReversedString.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReversedString {
+    public static void main(String[] args) {
+        //Reverse String – For Loop
+        Scanner scan =new Scanner(System.in);
+        System.out.println("Enter a name");
+        String stringName=scan.next();
+        String reversedName="";
+        for (int i =stringName.length()-1 ; i >=0 ; i--) {
+            reversedName=reversedName+stringName.charAt(i);
+        }
+        System.out.println(reversedName);
+
+        }
+    }
+
