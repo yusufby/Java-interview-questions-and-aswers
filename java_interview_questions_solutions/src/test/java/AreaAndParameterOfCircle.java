3b6c99c13502e62148b916838a2566c1f7846503
yusufby <yusuf89bayram@gmail.com>
2023-01-27 20:22:15 +0000

    display messages

---
+++ java_interview_questions_solutions/src/test/java/AreaAndParameterOfCircle.java
@@ -0,0 +1,12 @@
+public class AreaAndParameterOfCircle {
+    public static void main(String[] args) {
+        double radius=5.5 ;
+        double π=3.14;
+
+        double perimeter = 2 * radius * π;
+        double area = radius * radius * π;
+
+        System.out.println("perimeter is " + perimeter);
+        System.out.println("area is " + area);
+    }
+}
---
+++ java_interview_questions_solutions/src/test/java/AreaAndParameterOfRectangle.java
@@ -0,0 +1,12 @@
+public class AreaAndParameterOfRectangle {
+    public static void main(String[] args) {
+
+        double width=4.5;
+        double height=7.9;
+
+        double area = width * height;
+        double perimeter=2*(width+height);
+        System.out.println("Area is " + area);
+        System.out.println("Perimeter is "+perimeter);
+    }
+}
