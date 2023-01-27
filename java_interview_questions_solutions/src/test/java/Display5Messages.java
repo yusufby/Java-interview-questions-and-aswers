7ada0443068ea0e7bc548532ab360e115b4688e3
yusufby <yusuf89bayram@gmail.com>
2023-01-27 14:13:44 +0000

    display messages

---
+++ java_interview_questions_solutions/src/test/java/Display5Messages.java
@@ -0,0 +1,10 @@
+public class Display5Messages {
+    public static void main(String[] args) {
+
+        String message="Welcome to Java";
+        int i=6;
+        for (int j = 1; j < i; j++) {
+            System.out.println(message);
+        }
+    }
+}
